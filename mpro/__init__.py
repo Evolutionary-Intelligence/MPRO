0c3556d1ebb6afdf2c9e1fa3617224fd5f9bbfa8
Evolutionary-Intelligence (Qiqi Duan - 段琦琦) <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-09-17 18:30:31 +0800

    Update __init__.py

--- mpro/__init__.py
+++ mpro/__init__.py
@@ -1,8 +1,9 @@
 """MPRO is a Matrix-based pure-Python library of Population-based Randomized
     Optimization algorithm for complex (e.g., non-linear, non-convex, and
     multi-modality), single-objective, unconstrained, continuous problems.
-    Its goal is to provide *high-performance* and *elegant* implementations
-    for black-box and derivative-free optimization (BBO/DFO) entirely within
-    a *unified*, *standardized*, *easy-to-use* programming interface.
+    Its goal is to provide *high-performance* and *elegant* algorithmic
+    implementations for black-box and derivative-free optimization (BBO/DFO)
+    entirely under a *unified*, *standardized*, *easy-to-use* programming
+    interface.
 """
 import pypop7 as pp7
