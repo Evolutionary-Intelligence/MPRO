f331480234f5693ece2fde964d1bfe96657f1662
Evolutionary-Intelligence (Qiqi Duan - 段琦琦) <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-09-17 18:28:36 +0800

    Update __init__.py

--- mpro/__init__.py
+++ mpro/__init__.py
@@ -2,6 +2,6 @@
     optimization algorithm for complex (e.g., non-linear, non-convex, and
     multi-modality), single-objective, unconstrained, continuous problems.
     Its goal is to provide *high-performance* and *elegant* implementations
-    for derivative-free and black-box optimization entirely within a
-    *unified*, *standardized*, *easy-to-use* programming interface.
+    for black-box and derivative-free optimization (BBO/DFO) entirely within
+    a *unified*, *standardized*, *easy-to-use* programming interface.
 """
