f57835e2958fe55e9e44f6faac7fe75771067145
Evolutionary-Intelligence (Qiqi Duan - 段琦琦) <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2025-09-17 19:16:10 +0800

    Update __init__.py

--- mpro/__init__.py
+++ mpro/__init__.py
@@ -5,5 +5,10 @@
     implementations for black-box and derivative-free optimization (BBO/DFO)
     entirely under a *unified*, *standardized*, *easy-to-use* programming
     interface.
+
+    Although this open-source Python library is based on PyPop7, the main
+    difference lies on the former seeks to maximize the performance of *batch*
+    evaluations and update of randomized population.
 """
+# https://github.com/Evolutionary-Intelligence/pypop
 import pypop7 as pp7
